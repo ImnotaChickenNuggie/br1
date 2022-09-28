894aa2cce7412f3ed2e8f2c8c3cf681859dcc5f2
ImnotaChickenNuggie <brunohdzv@gmail.com>
2022-09-28 11:37:08 -0500

    creacion de barrel para header y footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Header, Footer } from './index';
 
 const layout = ({ children }) => {
 	return (
 		<div className='w-screen h-screen bg-cultured overflow-x-hidden'>
+			<Header />
 			{children}
+			<Footer />
 		</div>
 	);
 };
