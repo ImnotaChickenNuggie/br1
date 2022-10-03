a762088a3c68a8c6734fa9d7563860c313816506
ImnotaChickenNuggie <brunohdzv@gmail.com>
2022-10-02 21:30:16 -0500

    se crea el boton principal

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,18 @@ import Layout from '../components/layout';
 const IndexPage = ({ children }) => {
 	return (
 		<Layout>
-			<div className=' ml-20 w-10 h-10 bg-gold-500 '>
-				<p className='font-ichiji'>DeV</p>
-			</div>
+			<section className='h-full bg-cultured'>
+				<div className='mx-auto flex'>
+					<div className='mx-auto mt-12'>
+						<button className='bg-smoky p-3 btn-hexagon'>
+							<div className='bg-cultured btn-hexagon-border'>
+								<p className='pt-2'>Enviar</p>
+							</div>
+						</button>
+					</div>
+				</div>
+			</section>
+			<section className='h-full bg-emerald-400'></section>
 			{/* <Icon /> */}
 			<StaticImage alt='' src='../images/build.png' />
 			<p className='ml-20'>lorem ipsum</p>
